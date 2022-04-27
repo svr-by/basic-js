80e6034198bb9db9c92ae31370b1b123fe7c7f1e
Siarhei <siarhei.by@gmail.com>
2022-04-28 00:38:51 +0300

    feat: add solution сarbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  //throw new NotImplementedError('Not implemented');
+  if(!(typeof(sampleActivity) === 'string') || !isFinite(+sampleActivity) || !(+sampleActivity)) return false;
+
+  const LN2 = 0.693;
+  const K = LN2 / HALF_LIFE_PERIOD;
+
+  let age = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / K);
+
+  return (age > 0)? age : false;
 }
 
 module.exports = {
