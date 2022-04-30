7a6a5684ce54118b8391f077a3608c757bdbf21d
Siarhei <siarhei.by@gmail.com>
2022-04-30 11:13:54 +0300

    feat: add solution simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,26 +5,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new NotImplementedError('Not implemented');
-    // this.chain.push(`(${value})`);
-    // return this;
+    this.chain.push(`( ${ value } )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if(!isFinite(position) || Math.trunc(position) != position || (position < 1) || (position >= this.chain.length)) {
+      this.chain = [];
+      throw Error("You can't remove incorrect link!");
+    }
+    this.chain.splice((position - 1), 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let finish = this.chain.join('~~');
+    this.chain = [];
+    return finish;
   }
 };
 
